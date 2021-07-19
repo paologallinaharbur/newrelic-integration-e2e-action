4b96be66164119733b27df766251142d57aa073a
ivancorrales <ivan.corrales.solera@gmail.com>
2021-07-19 15:50:21 +0200

    feat: initial commit

--- spec-validator/cmd/main.go
+++ spec-validator/cmd/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"github.com/newrelic/infrastructure-agent/pkg/log"
+	"golang.org/x/tools/go/ssa/interp/testdata/src/os"
+)
+
+const(
+	specsPathEnvVar="SPECS"
+)
 
 func main(){
-	fmt.Println("validating specs...")
+	log.Info("running spec-validator")
+	paths:=os.Getenv(specsPathEnvVar)
+	log.Info("validating specs in path %s",paths)
 }
