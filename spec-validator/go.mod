2a9f6dc683dd3479653d5cb65c62f59741cbc51b
ivancorrales <ivan.corrales.solera@gmail.com>
2021-07-20 10:05:40 +0200

    feat: initial commit

--- spec-validator/go.mod
+++ spec-validator/go.mod
@@ -5,5 +5,6 @@ go 1.16
 require (
 	github.com/golangci/golangci-lint v1.40.1
 	github.com/sirupsen/logrus v1.8.1
+	github.com/stretchr/testify v1.7.0
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
 )
---
+++ spec-validator/pkg/model_test.go
@@ -0,0 +1,15 @@
+package pkg
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_ParseSpecFile(t *testing.T){
+	var sample = `
+		description: This is a demo
+	`
+	spec,err:=ParseSpecFile([]byte(sample))
+	assert.Nil(t, err)
+	assert.Equal(t, "This is a demo",spec.Description)
+}
