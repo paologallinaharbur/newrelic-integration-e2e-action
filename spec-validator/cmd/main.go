b2e8a5e04b0b4f4a7673afe25499635d98a57883
ivancorrales <ivan.corrales.solera@gmail.com>
2021-07-20 09:47:19 +0200

    feat: initial commit

--- spec-validator/cmd/main.go
+++ spec-validator/cmd/main.go
@@ -15,34 +15,27 @@ var log = logrus.New()
 
 const (
 	flagSpecPath = "spec_path"
+	flagVerboseMode = "verbose_mode"
 )
 
 func main() {
 	log.Info("running spec-validator")
 	specsPathPtr := flag.String(flagSpecPath, "", "Relative path to the spec file")
+	verboseModePtr := flag.Bool(flagVerboseMode, false, "If true the debug level is enabled")
 	flag.Parse()
 	specPath := *specsPathPtr
 	if specPath == "" {
 		os.Exit(1)
 	}
-	parent:=filepath.Base(specPath)
-	fmt.Println(parent)
-	var files []string
-	err := filepath.Walk(parent, func(path string, info os.FileInfo, err error) error {
-		files = append(files, path)
-		return nil
-	})
-	if err != nil {
-		panic(err)
-	}
-	for _, file := range files {
-		fmt.Println(file)
+	if *verboseModePtr{
+		log.SetLevel(logrus.DebugLevel)
 	}
 	content,err:=ioutil.ReadFile(specPath)
 	if err!=nil{
 		log.Error(err)
 		os.Exit(1)
 	}
+	log.Debug("parsing the content of the spec file")
 	pkg.ParseSpecFile(content)
 }
 
