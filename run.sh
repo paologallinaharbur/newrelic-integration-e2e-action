cd8879227e30afd7a9594dcff0c4aa2ec2cd1906
ivancorrales <ivan.corrales.solera@gmail.com>
2021-07-23 11:51:52 +0200

    add quotes to avoid word splitting

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 LICENSE_KEY=$1
 SPEC_PATH=samples/nri-powerdns/powerdns-e2e.yml
-make LICENSE_KEY="${LICENSE_KEY}" ROOT_DIR=$(PWD) SPEC_PATH=${SPEC_PATH} VERBOSE=true run
+make LICENSE_KEY="${LICENSE_KEY}" ROOT_DIR="$(PWD)" SPEC_PATH=${SPEC_PATH} VERBOSE=true run
